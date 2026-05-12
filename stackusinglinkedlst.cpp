9fde9dceaa2b5ce89381aa072d23d56a3f41ed31
Ilham224 <muhammadilham080406@gmail.com>
2026-05-12 09:00:39 +0700

    tambah fungsi

--- stackusinglinkedlst.cpp
+++ stackusinglinkedlst.cpp
@@ -26,3 +26,20 @@ public:
         top = newNode;
     }
     
+    void pop() {
+        if (top == nullptr) {
+            cout << "Stack underflow!" << endl;
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    int peek() {
+        if (top == nullptr) {
+            cout << "Stack is empty!" << endl;
+            return -1; // Return -1 to indicate stack is empty
+        }
+        return top->data;
+    }
