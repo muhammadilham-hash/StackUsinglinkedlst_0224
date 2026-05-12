4a876a7521b0afbc6b53a00b9cd44a95c50c36d5
Ilham224 <muhammadilham080406@gmail.com>
2026-05-12 08:59:18 +0700

    class stack

--- stackusinglinkedlst.cpp
+++ stackusinglinkedlst.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 
 using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};   
+      
+class Stack {
+private:
+    Node* top;
+public:
+    Stack() {
+        top = nullptr;
+    }       
+    void push(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+    }
+    
